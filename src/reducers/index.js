ac07955e7788becdef46834e129a48e0ab76c5e6
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-11-05 13:22:47 -0500

    reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,45 @@
 
 export const initialState = {
+    smurfs = [],
+    loading = false,
+    error = ""
 }
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action)=>{ 
+    switch(action.type){
+        case(FETCH_START):
+        return({
+          ...state,
+          smurfs: {},
+          loading:true,
+          error:''
+        });
+        case(FETCH_SUCCESS):
+      return({
+        ...state,
+        smurfs: action.payload,
+        loading: false,
+        error: ''
+      });
+      case(FETCH_ERROR):
+      return({
+        ...state,
+        smurfs:{},
+        loading: false,
+        error: action.payload
+      });
+      case ADD:
+          return {
+              ...state, smurfs: action.payload
+          };
+      case ERROR:
+          return {
+                error: action.payload
+        };
+       default:
+            return state;
+    }
+};
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
